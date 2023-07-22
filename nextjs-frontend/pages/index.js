8eed44bc96bcd7c4e61a535e73329f463ea31127
Deepak Kushwaha <deepakkushwaha@Deepaks-MacBook-Pro.local>
2023-07-22 20:07:39 +0530

    Changes for vercel deployment

--- nextjs-frontend/pages/index.js
+++ nextjs-frontend/pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const navigate = useRouter()
   return (
     <>
       <Head>
